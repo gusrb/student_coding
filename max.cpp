9d0c470540d794db7083e5bcff36bd0f47ec10fc
gusrb <48879298+gusrb@users.noreply.github.com>
2019-05-11 12:06:17 +0900

    Add files via upload

---
+++ max.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream> 
+using namespace std; 
+int main()
+{
+	int N,max,a[100];
+	cin >> N;
+	for (int i = 0; i < N; i++)
+		cin >> a[i];
+	max = a[0];
+for (int i = 1; i < N; i++)
+{
+	if (a[i] > max)
+		max = a[i];
+}
+cout << max;
+
+
+
+}
