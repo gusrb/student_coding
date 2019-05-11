c8be3e7573b0c6377903823769f4601e7c3745b8
gusrb <48879298+gusrb@users.noreply.github.com>
2019-05-11 12:49:25 +0900

    Add files via upload

---
+++ 삼각형만들기.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream> 
+using namespace std;
+
+void make_t(int idx, int n) {
+
+	if (idx == n)
+		return;
+
+	for (int i = 0; i < idx; i++)
+		cout << "*"; 
+	cout << endl;
+
+	make_t(idx + 1, n);
+
+	for (int i = 0; i < idx; i++)
+		cout << "*"; 
+	cout << endl;
+} 
+
+int main()
+{
+	int n; 
+	scanf_s("%d", &n);
+	make_t(1,n);
+}
---
+++ 재귀피보나치.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream> 
+using namespace std; 
+
+int fib(unsigned int n) {
+	if (n == 0 || n == 1)
+		return n; 
+	return fib(n - 1) + fib(n - 2);
+} 
+
+int main()
+{
+	int n; 
+	cin >> n;
+	cout<< fib(n);
+}
