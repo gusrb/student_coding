7e0e84d1b13f9688f4572b0f1b4310c807eadbea
gusrb <48879298+gusrb@users.noreply.github.com>
2019-05-11 12:47:15 +0900

    Add files via upload

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -1,4 +1,5 @@
-﻿#include <iostream> 
+﻿#include <iostream>  
+#include <algorithm>
 using namespace std; 
 int BinarySearch(int A[], int value, int low, int high) {
 	int mid; 
@@ -10,19 +11,45 @@ int BinarySearch(int A[], int value, int low, int high) {
 		return BinarySearch(A, value, mid + 1, high);
 	else
 		return mid;
+}  
+int max(int A[],int N) 
+{
+	int max;
+	max = A[0];
+	for (int i = 1; i < N; i++)
+		if (A[i] > max)
+			max = A[i]; 
+	return max;
 } 
+
+int min(int A[], int N)
+{
+	int min;
+	min = A[0];
+	for (int i = 1; i < N; i++)
+		if (A[i] < min)
+			min = A[i];
+	return min;
+}
 
 int main()
 {
-	int N, A[50],X; 
+	int N, A[50],X,m,n; 
 
 	cin >> N; 
 	for (int i = 0; i < N; i++) {
 		cin >> A[i];
 	}  
+	sort(A[0], A[50]);   
+
+
 
 	while (X == -1) { 
-		cin >> X; 
-		BinarySearch(A[50],X, A[0],)
-	}
+		cin >> X;  
+		m =  max(A[50], N); 
+		n = min(A[50], N);
+		BinarySearch(A[50], X, m, n);
+	} 
+	
+
 }
