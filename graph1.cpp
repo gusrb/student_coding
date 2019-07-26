4139bbc96b5b29a4428de63c8fc7f197f2c6598e
gusrb <48879298+gusrb@users.noreply.github.com>
2019-07-26 12:49:44 +0900

    Add files via upload

--- graph1.cpp
+++ graph1.cpp
@@ -1,28 +1,56 @@
-#include <iostream>
+#include<iostream> 
+#include<vector>  
 using namespace std;
-int main() {
-	int node, edge, a[10][10] = { 0 }, min = 2000, min_a[10][10] = { 0 }, c = 0;
-	cin >> node >> edge; 
-	for (int i = 0; i < 5; i++)
-	{
-		for (int j = 0; j < 3; j++)
-		{
-			cin >> a[i][j];
+
+#define MAXV 1000
+typedef struct vertex {
+	int y;
+	int w;
+} Vertex;
+vector<Vertex> graph[MAXV + 1];
+int v_num, e_num; 
+
+void print_graph()
+{
+	int min = graph[1][0].w; 
+
+	for (int i = 1; i < v_num + 1; i++) {
+		for (int j = 0; j < graph[i].size(); j++) {
+			if (min > graph[i][j].w)
+				min = graph[i][j].w;
+		}
+	} 
+
+	for (int i = 1; i < v_num + 1; i++) {
+		for (int j = 0; j < graph[i].size(); j++) {
+			if (graph[i][j].w == min) {
+				if (i < graph[i][j].y)
+					cout << i << " " << graph[i][j].y << " " << graph[i][j].w << endl;
+				else
+					cout << graph[i][j].y << " " << i << " " << graph[i][j].w << endl;
+			}
 		}
 	}
-	for (int i = 0; i < 5; i++)
-	{	
-		if (a[i][2] < min)
-		{
-			min = a[i][2];
-			
-		}
-	}  
-	for (int i = 0; i < 5; i++)
-	{
-		if (a[i][2] == min)
-		{ 
-			cout << a[i][0] << " " << a[i][1] << " " << a[i][2] << endl;
+} 
+
+void make_graph(bool directed)
+{
+	int x, y, w;
+	Vertex node;
+	cin >> v_num >> e_num;
+	for (int i = 0; i < e_num; i++) {
+		cin >> x >> y >> w;
+		node.y = y; node.w = w;
+		graph[x].push_back(node);
+		if (!directed) {
+			node.y = x;
+			graph[y].push_back(node);
 		}
 	}
+} 
+
+int main()
+{
+	make_graph(1); 
+	print_graph();
 }
