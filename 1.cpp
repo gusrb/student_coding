4071afc4dc858cfc82939e845eb39561ff2ddfa1
Jeon Hyeon Gyu <48879298+gusrb@users.noreply.github.com>
2019-08-02 12:49:11 +0900

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string> 
+#include <stdlib.h>
+using namespace std;
+int factorial(int n)
+{
+	if (n == 1)
+		return 1;
+
+	return n * factorial(n - 1);
+}
+
+int main()
+{
+	int n, length, back;
+	long long int sum = 0; 
+
+	cin >> n;
+	sum = factorial(n); 
+	string str = to_string(sum);
+	length = str.length(); 
+	for (int i = length-1; i > 0; i--)
+	{
+		if (str[i] != '0')
+		{ 
+			int a = str[i];
+			//printf("%c", str[i]);
+			cout << str[i];
+		} 
+
+	}
+
+
+
+
+	return 0;
+}
---
+++ 2.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>   
+using namespace std;
+int sum = 0, n , g ,d; 
+
+int di(int k)
+{ 
+	if (k != 0) { 
+		
+		n = n / k; 
+		d += n % k; 
+		sum += n;
+	} 
+	if (d >= k)
+	{
+		sum += d / k; 
+		d += n % k;
+	}
+
+	if (n < k)
+		return sum;
+	else
+		di(k);
+
+}
+
+int main()
+{
+	int  k;
+	cin >> g >> k;    
+	n = g;
+	cout << di(k)+g;
+
+	return 0;
+}
