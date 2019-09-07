84d09b50551cb6b840636d1e1860a8fb50625fb3
Jeon Hyeon Gyu <48879298+gusrb@users.noreply.github.com>
2019-09-07 11:40:15 +0900

    Add files via upload

---
+++ dfs_binary_tree.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream> 
+using namespace std;  
+int tree[100];
+void dfs(int node)
+{
+	if (tree[node] == 0)
+		return; 
+	cout << tree[node] << " ";
+	dfs(node * 2); 
+	dfs(node * 2 + 1);
+} 
+
+int main()
+{  
+	int n,index,node;
+	cin >> n; 
+	for (int i = 0; i < n; i++)
+	{ 
+		cin >> index;  
+		cin >> node;
+		tree[index] = node;
+		 
+	} 
+	dfs(1);
+
+}
---
+++ tree_calculate.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream> 
+using namespace std;  
+int t,treeNode; 
+char tree[100];
+
+void inOrder(int node)
+{
+	if (tree[node] == NULL)
+		return;  
+	inOrder(node * 2);
+	cout << tree[node] << " ";
+	inOrder(node * 2 + 1);
+}  
+
+void preOrder(int node) {
+	if (tree[node] == NULL)
+		return;
+	cout << tree[node] << " ";;
+	preOrder(node * 2);
+	preOrder(node * 2+1);
+}
+
+void postOrder(int node)
+{
+	if (tree[node] == NULL)
+		return; 
+	postOrder(node * 2);
+	postOrder(node * 2 + 1);
+	cout << tree[node] << " ";
+	
+}
+
+
+int main()
+{  
+	int n,index; 
+	char node;
+	cin >> n; 
+	for (int i = 0; i < n; i++)
+	{ 
+		cin >> index;  
+		cin >> node;
+		tree[index] = node;
+		 
+	}   
+
+	cout << "in-order : ";
+	inOrder(1);  
+
+	cout << endl<<"pre-order : ";
+	preOrder(1);  
+
+	cout <<endl<< "post-order : ";
+	postOrder(1);
+		
+}
