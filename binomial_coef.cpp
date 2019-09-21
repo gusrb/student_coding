a46cace8219067d0f60400df8229650ad85d93a4
Jeon Hyeon Gyu <48879298+gusrb@users.noreply.github.com>
2019-09-21 12:00:17 +0900

    Add files via upload

---
+++ binomial_coef.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream> 
+using namespace std;
+
+#define MAXN 100
+
+long binomial_coef(int n, int m) {
+	int i, j;
+	long bc[MAXN][MAXN];
+for (i = 0; i <= n; i++) bc[i][0] = 1;
+for (j = 0; j <= n; j++) bc[j][j] = 1;
+for (i = 1; i <= n; i++)
+for (j = 1; j < i; j++)
+	bc[i][j] = bc[i - 1][j - 1] + bc[i - 1][j];
+return (bc[n][m]);
+}
+
+int main()
+{
+	int n,k;
+	cin >> n>>k;
+	cout <<binomial_coef(n,k);
+
+}
+
