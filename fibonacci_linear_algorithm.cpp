eef54700f992ef9eb04fb2b4007bba302d3e0a44
Jeon Hyeon Gyu <48879298+gusrb@users.noreply.github.com>
2019-09-21 10:39:13 +0900

    Add files via upload

---
+++ fibonacci_linear_algorithm.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream> 
+using namespace std;    
+#define Max 100000000
+int fibScore[Max] = { 0 }; 
+
+int fib(unsigned int n) {
+	int i;
+	fibScore[0] = 0;
+	fibScore[1] = 1;
+	for (i = 2; i <= n; i++) {
+		fibScore[i] = fibScore[i - 1] +
+			fibScore[i - 2];
+	}
+	return fibScore[n];
+}
+
+
+int main()
+{
+	int a;
+	cin >> a; 
+	cout << fib(a);
+		
+} 
+
---
+++ guess_max.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream> 
+using namespace std;    
+#define Max 100000000
+int fibScore[Max] = { 0 }; 
+
+int fib(unsigned int n) {
+	if (n == 0 || n == 1 || n == 2)
+		return 1; 
+	if (n == 3)
+		return 2;
+	return fib(n - 1) + fib(n - 3)+fib(n-4);
+}
+
+int main()
+{
+	int a;
+	cin >> a; 
+	cout << fib(a);
+		
+} 
+
